505fa793dc5a8541bc3f24d047c790c0226b6480
Martin Metodiev <metodiev.martin@gmail.com>
2021-06-21 11:06:32 +0300

    Add service worker

--- configs/grunt/realFavicon.js
+++ configs/grunt/realFavicon.js
@@ -1,42 +1,64 @@
 module.exports = {
-  options: {
-    iconsPath: '/',
-    design: {
-      ios: {
-        pictureAspect: 'backgroundAndMargin',
-        backgroundColor: '#ffffff'
-      },
-      desktopBrowser: {},
-      windows: {
-        pictureAspect: 'noChange',
-        backgroundColor: '#da532c',
-        onConflict: 'override'
-      },
-      androidChrome: {
-        pictureAspect: 'backgroundAndMargin',
-        backgroundColor: '#ffffff',
-        themeColor: '#ffffff',
-        manifest: {
-          name: 'Martin’s work',
-          display: 'browser',
-          orientation: 'notSet',
+  favicons: {
+    src: 'src/images/icon.png',
+    dest: 'dist/assets/favicons',
+    options: {
+      iconsPath: '/',
+      design: {
+        ios: {
+          pictureAspect: 'noChange',
+          assets: {
+            ios6AndPriorIcons: false,
+            ios7AndLaterIcons: false,
+            precomposedIcons: false,
+            declareOnlyDefaultIcon: true
+          }
+        },
+        desktopBrowser: {
+          design: 'raw'
+        },
+        windows: {
+          pictureAspect: 'noChange',
+          backgroundColor: '#ef4d14',
           onConflict: 'override',
-          declared: true
+          assets: {
+            windows80Ie10Tile: false,
+            windows10Ie11EdgeTiles: {
+              small: false,
+              medium: true,
+              big: false,
+              rectangle: false
+            }
+          }
+        },
+        androidChrome: {
+          pictureAspect: 'noChange',
+          themeColor: '#ffffff',
+          manifest: {
+            name: 'Martin’s work',
+            display: 'standalone',
+            orientation: 'notSet',
+            onConflict: 'override',
+            declared: true
+          },
+          assets: {
+            legacyIcon: false,
+            lowResolutionIcons: false
+          }
+        },
+        safariPinnedTab: {
+          pictureAspect: 'blackAndWhite',
+          threshold: 64.0625,
+          themeColor: '#ef4d14'
         }
       },
-      safariPinnedTab: {
-        pictureAspect: 'silhouette',
-        themeColor: '#5bbad5'
+      settings: {
+        scalingAlgorithm: 'Lanczos',
+        errorOnImageTooSmall: false,
+        readmeFile: false,
+        htmlCodeFile: false,
+        usePathAsIs: false
       }
-    },
-    settings: {
-      scalingAlgorithm: 'Lanczos',
-      errorOnImageTooSmall: false
     }
-  },
-
-  task: {
-    src: 'src/images/icon.png',
-    dest: 'dist/assets/favicons'
   }
 };
